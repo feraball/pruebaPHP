2ba6d8f50d89510dc963296320138dd06a8a43e9
feraball <balladares.fernando.92@gmail.com>
2017-12-20 13:12:38 -0500

    commit a produccion

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+	<body style="background-color:grey; color:red">
+		<?php
+			echo '<p>Fernando Balladares</p>';
+			echo '<p>RAMA PRODUCCION</p>';
+		?>
+	<a href="about.html">Acerca de</a>
+	</body>
+</html>
